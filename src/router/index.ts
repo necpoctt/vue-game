2b7dc72c5504d682576546128a9c288172833a53
Howard <howard.liao@cleflex.com>
2023-10-13 16:06:41 +0800

    removd no use code

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,22 +8,11 @@ export const constantRoutes = [
   },
 ];
 
-/**
- * 创建路由
- */
 const router = createRouter({
   history: createWebHistory(),
   routes: constantRoutes,
-  // 刷新时，滚动条位置还原
+
   scrollBehavior: () => ({ left: 0, top: 0 }),
 });
-
-/**
- * 重置路由
- */
-export function resetRouter() {
-  router.replace({ path: '/login' });
-  location.reload();
-}
 
 export default router;
